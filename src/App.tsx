cbfbb5da2a52b827967fa1e6a193986eb08eb793
George Ofogba <ofogbageorge3@gmail.com>
2025-01-16 13:10:35 +0100

    Project started

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div>
+      <h1 className=" text-2xl font-bold text-center">
+        Hello from Jojo Bankify
+      </h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout.tsx';
+import AuthLayout from './layouts/AuthLayout.tsx';
+import SignIn from './pages/auth/SignIn.tsx';
+import Home from './pages/Home.tsx';
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
